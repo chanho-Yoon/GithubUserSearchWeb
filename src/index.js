fb51416a93eb6dde3448d7826ea5e8c69c87ea68
ChanhoYoon <chyoon0512@gmail.com>
2021-06-26 15:56:38 +0900

    Add : local 조회 기능 및 삭제 기능 구현

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import './styles/style.scss';
 const getSearchValue = document.querySelector('.search-input');
 const getSearchIconValue = document.querySelector('.search-icon');
 const userUlElement = document.querySelector('.api-list');
+const localUlElement = document.querySelector('.local-list');
+
+const LOCAL_KEY = 'favoritesUser';
 let usersData = null;
+let localUsersData = null;
 
 function init() {
    const tabs = document.querySelectorAll('[data-tab-target]');
@@ -19,14 +23,86 @@ function init() {
             tabc_all.classList.remove('active');
          });
          target.classList.add('active');
+         ulUserElementReset();
       });
    });
-
    // 처음 실행 시 유저 목록이 비어있음
-   ulElementReset();
+   ulUserElementReset();
+   createLocalList();
 }
 
-// user/api 검색된 단어로 유저 정보 가져오는 함수
+// 즐겨찾기 함수 , 로컬 스토리지에 저장 -------------------------------------------------------
+function userFavorites( e, item ) {
+   let addItem = { id: item.id, login: item.login, avatar_url: item.avatar_url };
+   const className = e.target.parentNode.className;
+
+   if (className.includes('active')) {
+      e.target.parentNode.classList.remove('active');
+      // localStorage에 삭제한 아이템을 제외하고 다시 set
+      let removeStorage = JSON.parse(localStorage.getItem(LOCAL_KEY)).filter(( list ) => list.id !== item.id);
+      localStorage.setItem(LOCAL_KEY, JSON.stringify(removeStorage));
+      createLocalList();
+   } else {
+      e.target.parentNode.classList.add('active');
+      let getLocalStorageItem = localStorage.getItem(LOCAL_KEY);
+      getLocalStorageItem = JSON.parse(getLocalStorageItem);
+
+      if (getLocalStorageItem === null)
+         localStorage.setItem(LOCAL_KEY, JSON.stringify([addItem]));
+      else {
+         getLocalStorageItem.push(addItem);
+         localStorage.setItem(LOCAL_KEY, JSON.stringify(getLocalStorageItem));
+      }
+      createLocalList();
+   }
+}
+
+// Enter 입력 발생 시
+function searchAction( e ) {
+   if (e.key === 'Enter') {
+      if (e.target.value === '')
+         ulUserElementReset();
+      else
+         searchUser(e.target.value);
+   }
+   if (e.type === 'click') {
+      if (e.target.value === '')
+         ulUserElementReset();
+      else
+         searchUser(e.target.value);
+   }
+}
+
+// 유저 목록 배열이 비어있을 때 실행할 함수
+function ulUserElementReset() {
+   userUlElement.innerHTML = `
+      <li>
+         <h1> 유저 목록이 비었습니다.</h1>
+      </li>
+   `;
+   getSearchValue.value = '';
+}
+
+// 로컬 목록 배열이 비어있을 때 실행할 함수
+function ulLocalElementReset() {
+   localUlElement.innerHTML = `
+      <li>
+         <h1> 유저 목록이 비었습니다.</h1>
+      </li>
+   `;
+}
+
+// 유저 재검색을 위한 초기화
+function ulUserElementEmpty() {
+   userUlElement.innerHTML = ``;
+}
+
+// 로컬 재검색을 위한 초기화
+function ulLocalElementEmpty() {
+   localUlElement.innerHTML = ``;
+}
+
+// user/api 검색된 단어로 유저 정보 및 리스트 만드는 함수 -------------------------------------------------------
 async function searchUser( searchWord = null ) {
    const octokit = new Octokit({ auth: process.env.GIT_API });
    const response = await octokit.request('GET /search/users', {
@@ -46,13 +122,13 @@ async function searchUser( searchWord = null ) {
    });
 
    // 재검색을 위해 초기화
-   ulElementEmpty();
+   ulUserElementEmpty();
 
    let prev = '!@#dumyValue';
    let startWord = prev;
 
    if (usersData.length !== 0) {
-      let getLocalStorageItem = localStorage.getItem('favoritesUser');
+      let getLocalStorageItem = localStorage.getItem(LOCAL_KEY);
       getLocalStorageItem = JSON.parse(getLocalStorageItem);
 
       usersData.forEach(( item ) => {
@@ -100,65 +176,65 @@ async function searchUser( searchWord = null ) {
             userUlElement.appendChild(liEl);
          }
       );
-// dom에 element가 다 그려진 후 클릭이벤트 등록 및 실행하는 함수 실행
    } else {
-      ulElementReset();
+      ulUserElementReset();
    }
 }
 
-// 즐겨찾기 함수 , 로컬 스토리지에 저장
-function userFavorites( e, item ) {
-   let addItem = { id: item.id, login: item.login, avatar_url: item.avatar_url };
-   const className = e.target.parentNode.className;
+// 즐겨찾기 리스트 생성하는 함수 -------------------------------------------------------
+function createLocalList() {
+   ulLocalElementReset();
+   localUsersData = localStorage.getItem(LOCAL_KEY);
+   localUsersData = JSON.parse(localUsersData);
+   let prev = '!@#dumyValue';
+   let startWord = prev;
 
-   if (className.includes('active')) {
-      e.target.parentNode.classList.remove('active');
-      // localStorage에 삭제한 아이템을 제외하고 다시 set
-      let removeStorage = JSON.parse(localStorage.getItem('favoritesUser')).filter(( list ) => list.id !== item.id);
-      localStorage.setItem('favoritesUser', JSON.stringify(removeStorage));
+   console.log(localUsersData);
+   if (localUsersData !== null && localUsersData.length !== 0) {
+      ulLocalElementEmpty();
+      localUsersData.forEach(( item ) => {
+            const liEl = document.createElement('li');
+            const pEl = document.createElement('p');
+            const imgEl = document.createElement('img');
+            const spanEl = document.createElement('span');
+            const btnEl = document.createElement('button');
+            const iEl = document.createElement('i');
+            // 이전 첫 글자와 다른 첫 번째 경우에만 실행하는 조건문
+            imgEl.src = item.avatar_url;
+            imgEl.alt = 'profile image';
+            spanEl.innerText = item.login;
 
+            btnEl.className = 'btn active';
+            iEl.className = 'far fa-star';
+
+            if (prev !== item.login.slice(0, 1)) {
+               prev = item.login.slice(0, 1);
+               startWord = prev;
+
+               pEl.className = 'item-start-word';
+               pEl.innerText = startWord;
+
+               imgEl.src = item.avatar_url;
+               imgEl.alt = 'profile image';
+               spanEl.innerText = item.login;
+
+               liEl.appendChild(pEl);
+               liEl.appendChild(imgEl);
+               liEl.appendChild(spanEl);
+               liEl.appendChild(btnEl);
+            } else {
+               liEl.appendChild(imgEl);
+               liEl.appendChild(spanEl);
+               liEl.appendChild(btnEl);
+            }
+            btnEl.appendChild(iEl);
+            btnEl.addEventListener('click', ( e ) => userFavorites(e, item));
+            localUlElement.appendChild(liEl);
+         }
+      );
    } else {
-      e.target.parentNode.classList.add('active');
-      let getLocalStorageItem = localStorage.getItem('favoritesUser');
-      getLocalStorageItem = JSON.parse(getLocalStorageItem);
-
-      if (getLocalStorageItem === null)
-         localStorage.setItem('favoritesUser', JSON.stringify([addItem]));
-      else {
-         getLocalStorageItem.push(addItem);
-         localStorage.setItem('favoritesUser', JSON.stringify(getLocalStorageItem));
-      }
+      ulLocalElementReset();
    }
-}
-
-// Enter 입력 발생 시
-function searchAction( e ) {
-   if (e.key === 'Enter') {
-      if (e.target.value === '')
-         ulElementReset();
-      else
-         searchUser(e.target.value);
-   }
-   if (e.type === 'click') {
-      if (e.target.value === '')
-         ulElementReset();
-      else
-         searchUser(e.target.value);
-   }
-}
-
-// 유저 목록 배열이 비어있을 때 함수
-function ulElementReset() {
-   userUlElement.innerHTML = `
-      <li>
-         <h1> 유저 목록이 비었습니다.</h1>
-      </li>
-   `;
-}
-
-// 재검색을 위한 초기화
-function ulElementEmpty() {
-   userUlElement.innerHTML = ``;
 }
 
 init();
